c6cf6fd9ef95cddf13e539f57a1ad2805d1eb6e3
Andrew Barron <hello@awmb.uk>
2020-05-18 13:08:07 +0100

    Disabling notification email

--- processing/formHandler.php
+++ processing/formHandler.php
@@ -315,9 +315,10 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             shell_exec("php cron.php");
 
             // send notification email
-            notificationEmail($showDetails["name"] . " submitted",
-                "A new show has been submitted:\n\n" .
-                $showDetails["name"] . " for " . $_POST["date"] . ".");
+            // TODO fix email
+//            notificationEmail($showDetails["name"] . " submitted",
+//                "A new show has been submitted:\n\n" .
+//                $showDetails["name"] . " for " . $_POST["date"] . ".");
 
             // run the cron job just now asynchronously
             exec("php " . __DIR__ . "/cron.php");
