ecc74b159a1a71c8f3b5ea8ca61bc67ca4be0dbb
Andrew Barron <hello@awmb.uk>
2020-08-17 19:59:28 +0100

    Added some trace logs to part of cron

--- processing/cron.php
+++ processing/cron.php
@@ -6,15 +6,20 @@ use Aws\S3\Exception\S3Exception;
 use Flow\FileOpenException;
 use Flow\Uploader;
 
+logWithLevel("info", "Cron requested");
+
 if (mkdir(__DIR__ . '/showSubmissionsCronRunning.lock', 0700)) {
+    logWithLevel("info", "Cron running");
     $config = require 'config.php';
     $connections = require 'databaseConnections.php';
     require __DIR__ . "/../vendor/autoload.php";
+    logWithLevel("info", "Cron: Requires complete");
 
 // get the shows due to publish, delete, and move to S3
     $showsDueToPublish = $connections["submissions"]->query("SELECT * FROM submissions WHERE `end-datetime` < CURRENT_TIMESTAMP AND `deletion-datetime` IS NULL");
     $showsDueForDeletion = $connections["submissions"]->query("SELECT * FROM submissions WHERE `deletion-datetime` < CURRENT_TIMESTAMP");
     $showsWaitingForS3 = $connections["submissions"]->query("SELECT * FROM submissions WHERE file_location = 'waiting'");
+    logWithLevel("trace", "Cron: Queried for details");
 
 // prepare queries for removing published shows
     $removePublishedTags = $connections["submissions"]->prepare("DELETE FROM tags WHERE submission = ?");
@@ -25,28 +30,39 @@ if (mkdir(__DIR__ . '/showSubmissionsCronRunning.lock', 0700)) {
 
 // prepare query for noting in database that a show is now in S3
     $noteShowMovedToS3 = $connections["submissions"]->prepare("UPDATE submissions SET file_location = 's3' WHERE id = ?");
+    logWithLevel("trace", "Cron: Prepared queries");
 
 // if there are any shows to publish, for each show
     if ($showsDueToPublish->num_rows > 0) {
+        logWithLevel("trace", "Cron: There are shows to publish");
         while ($show = $showsDueToPublish->fetch_assoc()) {
+            logWithLevel("trace", "Cron: Processing the publication of a show");
             if ($show["file_location"] == "local") { // if file is in local storage, load it
+                logWithLevel("trace", "Cron: Show is local");
                 $showFile = new CURLFile($config["uploadFolder"] . "/" . $show["file"]);
+                logWithLevel("trace", "Cron: CURLFile made");
             } else if ($show["file_location"] == "s3") { // if file is in S3
+                logWithLevel("trace", "Cron: Show is in S3");
                 try {
+                    logWithLevel("trace", "Cron: Trying to transfer show");
                     // Save object to a file
                     $result = $connections["s3"]->getObject(array(
                         'Bucket' => $config["s3Bucket"],
                         'Key' => "shows/" . $show["file"],
                         'SaveAs' => $config["tempDirectory"] . "/" . $show["file"]
                     ));
+                    logWithLevel("trace", "Cron: Show transferred");
                 } catch (S3Exception $e) {
                     logWithLevel("error", "Couldn't get " . $show["file"] . " from S3. Error:\n" . $e->getMessage());
                 }
 
                 // open the file from S3 as a CURLFile
                 $showFile = new CURLFile($config["tempDirectory"] . "/" . $show["file"]);
+                logWithLevel("trace", "Cron: CURLFile made");
             } else if ($show["file_location"] == "waiting") { // if the file is waiting to go to S3
+                logWithLevel("trace", "Cron: Show in waiting area");
                 $showFile = new CURLFile($config["waitingUploadsFolder"] . "/" . $show["file"]);
+                logWithLevel("trace", "Cron: CURLFile made");
             }
 
             // basic data
@@ -55,18 +71,25 @@ if (mkdir(__DIR__ . '/showSubmissionsCronRunning.lock', 0700)) {
                 'name' => $show["title"],
                 'description' => $show["description"]
             );
+            logWithLevel("trace", "Cron: Prepared basic show info");
 
             // if there's an image
             if (!empty($show["image"])) {
+                logWithLevel("trace", "Cron: There's an image");
                 // turn the blob into a PNG
                 $image = imagecreatefromstring($show["image"]);
+                logWithLevel("trace", "Cron: PNG made");
                 if ($image !== false) {
                     imagepng($image, $config["tempDirectory"] . "/img.png");
+                    logWithLevel("trace", "Cron: Saved image to temp file");
                     $imagePNG = new CURLFile($config["tempDirectory"] . "/img.png");
+                    logWithLevel("trace", "Cron: Made CURLFile");
                     imagedestroy($image);
+                    logWithLevel("trace", "Cron: Destroyed image variable");
 
                     // add the image to the POST data
                     $postData['picture'] = $imagePNG;
+                    logWithLevel("trace", "Cron: Added image to POST data");
                 }
             }
 
@@ -77,25 +100,34 @@ if (mkdir(__DIR__ . '/showSubmissionsCronRunning.lock', 0700)) {
                 // TODO handle this
                 logWithLevel("error", "Couldn't get tags of show to publish. " . $showTagsQuery->error);
             }
+            logWithLevel("trace", "Cron: Queried for show tags");
             $tags = mysqli_fetch_all(mysqli_stmt_get_result($showTagsQuery));
+            logWithLevel("trace", "Cron: Got array of show tags");
 
             // add each tag to the POST data
             for ($i = 0; $i < sizeof($tags); $i++) {
+                logWithLevel("trace", "Cron: Processing a tag");
                 $postData["tags-" . $i . "-tag"] = $tags[$i][0];
+                logWithLevel("trace", "Cron: Added tag to POST data");
             }
 
             // set up cURL
             $curl = curl_init('https://api.mixcloud.com/upload/?access_token=' . $config["mixcloudAccessToken"]);
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($curl, CURLOPT_POSTFIELDS, $postData);
+            logWithLevel("trace", "Cron: Set up CURL");
 
             // execute the cURL POST
             $response = json_decode(curl_exec($curl), true);
+            logWithLevel("trace", "Cron: Executed CURL. Response: \n" . json_encode($response));
 
             // close the request
             curl_close($curl);
+            logWithLevel("trace", "Cron: Closed CURL");
 
             if (isset($response["result"]["success"]) && $response["result"]["success"]) {
+                logWithLevel("trace", "Cron: CURL was a success");
+
                 // TODO error handling for queries
                 // remove tags for the show - we don't need them now
                 $removePublishedTags->bind_param("i", $show["id"]);
