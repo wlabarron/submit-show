6bae92e469377882342873e7bd8246c3204cf18a
Andrew Barron <hello@awmb.uk>
2025-08-10 13:05:26 +0100

    File upload page

--- upload.php
+++ upload.php
@@ -1,108 +1,46 @@
-<?php
-use Flow\Basic;
-use Flow\Config;
-use Flow\Request;
-use submitShow\Database;
-use submitShow\Recording;
+<?php 
+require './processing/promptLogin.php'; 
+$config = require './processing/config.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require "./components/head.html"; ?>
+</head>
+<body>
+<?php require "./components/noscript.html"; ?>
 
-require_once 'processing/requireAuth.php';
-require_once 'vendor/autoload.php';
-require_once 'processing/Database.php';
-require_once 'processing/Recording.php';
-require_once 'processing/Input.php';
-require_once 'processing/Storage.php';
-$config = require 'processing/config.php';
-
-$database  = new Database();
-$recording = new Recording();
-
-// prepare the input from the form
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $name      = Input::sanitise($_POST["name"]);
-    $presenter = Input::sanitise($_POST["presenter"]);
-    $date      = Input::sanitise($_POST["date"]);
-} else if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    $name      = Input::sanitise($_GET["name"]);
-    $presenter = Input::sanitise($_GET["presenter"]);
-    $date      = Input::sanitise($_GET["date"]);
-} else {
-    // Invalid request type.
-    http_response_code(406);
-    exit();
-}
-
-try {
-    $recording->setName($name);
-    $recording->setPresenter($presenter);
-    $recording->setStart($date);
-} catch (Exception $exception) {
-    error_log("Invalid show metadata: " . $exception->getMessage());
-    http_response_code(406);
-    exit;
-}
-
-$flowConfig = new Config();
-$flowConfig->setTempDir($config["tempDirectory"]);
-$request = new Request();
-
-try {
-    $recording->setFileExtension($request->getFileName());
-    $extension = $recording->getExtension();
-} catch (Exception $exception) {
-    error_log($exception->getMessage());
-    http_response_code(500);
-    exit;
-}
-
-if ($extension !== "mp3" &&
-    $extension !== "m4a" &&
-    $extension !== "mp4" &&
-    $extension !== "aac") {
-    // cancel upload
-    http_response_code(406);
-    exit;
-} else if ($request->getTotalSize() > $config["maxShowFileSize"]) { // if the file is too large
-    // cancel upload
-    http_response_code(406);
-    exit;
-}
-
-try {
-    $fileName = $recording->getFileName();
-} catch (Exception $exception) {
-    error_log($exception->getMessage());
-    http_response_code(500);
-    exit;
-}
-
-// set the path to upload to
-$uploadPath = $config["holdingDirectory"] . "/" . $fileName;
-if (!Storage::createParentDirectories($uploadPath)) {
-    error_log("Failed to create parent directories for  " . $uploadPath);
-    http_response_code(500);
-    exit;
-}
-
-// If this is the final chunk of the file
-if (Basic::save($uploadPath, $flowConfig, $request)) {
-    try {
-        // Write metadata about the show into the file
-        $removingMetadataLocation = $uploadPath . "-meta." . $extension;
-        shell_exec("ffmpeg -i \"$uploadPath\" -map_metadata -1 -metadata title=\"" . $recording->getName() . " " . $recording->get6DigitStartDate() . "\" -metadata artist=\"" . $recording->getPresenter() . "\" -c:v copy -c:a copy \"$removingMetadataLocation\"");
-        // move metadata-removed file back to the upload path
-        rename($removingMetadataLocation, $uploadPath);
-    } catch (Exception $e) {
-        error_log($e->getMessage());
-        http_response_code(500);
-        exit;
-    }
-
-    // Log upload completed
-    try {
-        if (!empty($_SESSION['samlNameId']))
-            $database->log($_SESSION['samlNameId'], "upload", $fileName);
-    } catch (Exception $e) {
-        error_log("Failed to log file upload action from " . $_SESSION["samlNameId"] . ".");
-    }
-}
-
+<div class="container">
+    <h1 class="h3">Upload file</h1>
+    <form>
+        <div class="form-group" id="showFileInputGroup">
+            <label for="showFileInput">Show file</label>
+            <input type="file" class="form-control" id="showFileInput" aria-describedby="showFileHelp"
+                   name="showFile" accept="audio/mpeg,audio/MPA,audio/mpa-robust,.mp3,.m4a,.mp4,.aac,audio/aac,audio/aacp,audio/3gpp,audio/3gpp2,audio/mp4,audio/mp4a-latm,audio/mpeg4-generic" required>
+            <small id="showFileHelp" class="form-text text-muted">
+                You can upload MP3 or M4A (AAC) files up to  <?php echo $config["maxShowFileSizeFriendly"]; ?>.
+            </small>
+        </div>
+        
+        <button type="submit" class="btn btn-primary mt-2">Upload and continue</button>
+    </form>
+    
+    <script>
+        // Warn the user before they navigate away
+        window.onbeforeunload = function () { return true;};
+        
+        const showFileInput = document.getElementById("showFileInput");
+        
+        showFileInput.addEventListener('change', function () {
+            const file = showFileInput.files[0];
+            if (file.size > <?php echo $config["maxShowFileSize"]; ?>) {
+                showFileInput.setCustomValidity("File too large. Maximum size is <?php echo $config["maxShowFileSizeFriendly"]; ?>.");
+            } else {
+                showFileInput.setCustomValidity("");
+            }
+            
+            showFileInput.reportValidity();
+        });
+    </script>
+</body>
+</html>
