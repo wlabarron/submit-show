6f7fc14759fbd8b402783261e8c1e565681b374d
Andrew Barron <hello@awmb.uk>
2025-08-10 13:16:39 +0100

    Rename file upload page and create file upload data page

---
+++ upload-data.php
@@ -0,0 +1,57 @@
+<?php 
+require './processing/promptLogin.php'; 
+$config = require './processing/config.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require "./components/head.html"; ?>
+</head>
+<body>
+<?php require "./components/noscript.html"; ?>
+
+<div class="container">
+    <h1 class="h3">About your upload</h1>
+    <form>
+        <div class="form-group">
+            <label for="date">Show date</label>
+            <input type="date" class="form-control" id="date" aria-describedby="dateHelp" name="date" required
+                   placeholder="YYYY-MM-DD" min="1970-01-01">
+            <small id="dateHelp" class="form-text text-muted">
+                Enter the first broadcast date of this show.
+            </small>
+        </div>
+        
+        <div class="form-group">
+            <label for="end">End time</label>
+            <input type="time" class="form-control" id="end" aria-describedby="endHelp"
+                   name="end" placeholder="HH:MM" required>
+            <small id="endHelp" class="form-text text-muted">
+                Enter the time this show ended or will finish when broadcast on air. This doesn't need to be
+                to-the-minute; if your show's time slot is 12:00-14:00, you can enter 14:00 here, even if you
+                finished at 13:56.
+            </small>
+        </div>
+        
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="endNextDay" value="false" id="endSameDay" checked>
+            <label class="form-check-label" for="endSameDay">
+                My show finished on the same day it started
+            </label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="endNextDay" value="true" id="endNextDay">
+            <label class="form-check-label" for="endNextDay">
+                My show runs over midnight, finishing the day after it started
+            </label>
+        </div>
+        
+        <button type="submit" class="btn btn-primary mt-3">Continue</button>
+    </form>
+    
+    <script>
+        // Warn the user before they navigate away
+        window.onbeforeunload = function () { return true;};
+    </script>
+</body>
+</html>
