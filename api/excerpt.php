f30ebea2e1f79dd460c5d7d4e97a809331e07ac1
Andrew Barron <hello@awmb.uk>
2025-08-11 11:16:11 +0100

    Configurable excerpt duration

--- api/excerpt.php
+++ api/excerpt.php
@@ -20,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] === "GET" && !empty($_GET["file"]) && !empty($_GE
         header("Content-Disposition: inline");
         header("Content-Transfer-Encoding: binary"); 
         header("Content-Type: audio/*"); 
-        passthru("ffmpeg -y -hide_banner -loglevel error  -i \"$filePath\" -ss 0 -t 600 -c copy -f mp3 -");
+        passthru("ffmpeg -y -hide_banner -loglevel error  -i \"$filePath\" -ss 0 -t \"{$config["serverRecordings"]["auditionTime"]}\" -c copy -f mp3 -");
         exit;
     } else {
         http_response_code(403);
